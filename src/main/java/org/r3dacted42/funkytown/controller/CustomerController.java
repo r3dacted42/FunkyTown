81c27737e9975e3634332863f5c82eb40ed05596
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2024-11-05 11:16:11 +0530

    customer create endpoint added to controller

--- src/main/java/org/r3dacted42/funkytown/controller/CustomerController.java
+++ src/main/java/org/r3dacted42/funkytown/controller/CustomerController.java
@@ -1,6 +1,13 @@
 package org.r3dacted42.funkytown.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.r3dacted42.funkytown.dto.CustomerRequest;
+import org.r3dacted42.funkytown.dto.CustomerResponse;
+import org.r3dacted42.funkytown.service.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/customers")
 @RequiredArgsConstructor
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<String> createCustomer(@RequestBody @Valid CustomerRequest request) {
+        return ResponseEntity.ok(customerService.createCustomer(request));
+    }
 }
